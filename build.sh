53ae9cea541f1fa93d7e66bb166f54a363da233c
Fonzzy1 <alfiechadwick@hotmail.com>
2023-12-18 11:08:51 +1100

    Move some stuff to idea

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-if [[ $(git status --porcelain) ]]; then
-    echo "There are uncommitted or unpushed changes. Please commit or push your changes before running this script."
-    exit 1
-fi
-
 pip install \
     matplotlib \
     numpy \
@@ -12,6 +7,6 @@ if ! R -e 'blogdown::build_site(build_rmd = TRUE)'; then
     exit 1
 fi
 
-if ! R -e 'blogdown::serve_site()'; then
+R -e 'blogdown::serve_site()'
 
 
