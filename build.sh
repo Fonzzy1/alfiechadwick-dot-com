0609dba24e5e9528646c2ee40328cd22da17661f
Fonzzy1 <alfiechadwick@hotmail.com>
2024-01-26 13:50:32 +1100

    Add R package installations to build script
    Extended R and catppuccin support in build script and post
    
    The build script now includes installation of the devtools R package,
    the catppuccin theme for R from GitHub, and the patchwork package.
    In the post, thematic R code blocks and a bar plot example have been
    added to demonstrate the R setup and the catppuccin theme application.
    The post explores the factors influencing song popularity in the Triple J
    Hottest 100 countdown.

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ pip install \
     pandas \
     catppuccin-matplotlib
 
+R -e 'install.packages("devtools")'
+R -e 'devtools::install_github("albert-ying/catppuccin")'
+R -e 'install.packages("patchwork")'
 
 if ! R -e 'blogdown::build_site(build_rmd = "md5sum")'; then
     exit 1
