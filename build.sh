d19adbce372d5db56de981f2bb64178747f39a99
Fonzzy1 <alfiechadwick@hotmail.com>
2024-01-28 12:13:21 +1100

    checkpoint

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-#pip install matplotlib numpy pandas catppuccin-matplotlib
+if [ "$1" = "--init" ]; then
+  # Run the commands
+  pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
 
-#R -e 'install.packages("devtools")'
-#R -e 'devtools::install_github("albert-ying/catppuccin")'
-#R -e 'install.packages("patchwork")'
-#R -e 'install.packages("jsonlite")'
+  R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
+  R -e 'install.packages("xgboost")'
+  R -e 'devtools::install_github("albert-ying/catppuccin")'
+fi
 
 if ! R -e 'blogdown::build_site(build_rmd = "md5sum")'; then
     exit 1
