e019a9b42183af17d228653c32ba63efa727be79
Fonzzy1 <alfiechadwick@hotmail.com>
2024-03-15 14:07:13 +1100

    ODE solver post

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
-if [ "$1" = "--init" ]; then
-  # Run the commands
-  pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
+for arg in "$@"
+do
+    if [ "$arg" = "--init" ]; then
+        # Run the initialisation commands
+        pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
 
-  R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
-  R -e 'install.packages("xgboost")'
-  R -e 'devtools::install_github("albert-ying/catppuccin")'
-fi
- 
+        R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
+        R -e 'install.packages("xgboost")'
+        R -e 'devtools::install_github("albert-ying/catppuccin")'
+    fi
+done
+
 R -e 'blogdown::build_site(build_rmd = "md5sum")'
 
-R -e 'blogdown::serve_site()'
-
-
+for arg in "$@"
+do
+    if [ "$arg" = "--serve" ]; then
+        R -e 'blogdown::serve_site()'
+    fi
+done
