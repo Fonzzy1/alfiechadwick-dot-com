b105712070af1d5e93fbc75537c4290d1315c780
Fonzzy1 <alfiechadwick@hotmail.com>
2025-03-24 17:22:22 +1100

    Initial builds

--- build.sh
+++ build.sh
@@ -1,20 +1,4 @@
-for arg in "$@"
-do
-    if [ "$arg" = "--init" ]; then
-        # Run the initialisation commands
-        pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
-
-        R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
-        R -e 'install.packages("xgboost")'
-        R -e 'devtools::install_github("albert-ying/catppuccin")'
-    fi
-done
-
-R -e 'blogdown::build_site(build_rmd = "md5sum")'
-
-for arg in "$@"
-do
-    if [ "$arg" = "--serve" ]; then
-        R -e 'blogdown::serve_site()'
-    fi
-done
+pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
+R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
+R -e 'install.packages("xgboost")'
+R -e 'devtools::install_github("albert-ying/catppuccin")'
--- content/about.md
+++ content/about.md
@@ -1,6 +1,7 @@
 ---
-title: "About"
-date: "2021-04-23T21:48:51-07:00"
+title: About
+date: '2021-04-23T21:48:51-07:00'
 ---
+
 
 Hiya, I'm Alfie a techie working in Melbourne, Australia. I often find myself writing up little projects in my spare time so I may as well put them out there.
