fb34d8b08e436b843b098e025f0da375a8bed3e5
Fonzzy1 <alfiechadwick@hotmail.com>
2025-03-26 15:38:13 +1100

    Remove .RData, .Rhistory, and .hugo_build.lock files

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
-R -e 'install.packages(c("devtools", "patchwork","jsonlite"))'
-R -e 'install.packages("xgboost")'
-R -e 'devtools::install_github("albert-ying/catppuccin")'
+R -e 'install.packages(c("jsonlite", "xgboost", "remotes"))'
+R -e 'remotes::install_github("albert-ying/catppuccin")'
