e9acce2ed8114c810d10ef34219324b9495c0a2b
Alfie Chadwick <75963032+Fonzzy1@users.noreply.github.com>
2025-03-26 15:08:04 +1100

    quarto (#23)
    * Starting convert to quarto
    
    * Add .env to .gitignore file
    
    * Initial builds
    
    * Working on math fixes
    
    * Fixed the themeing
    
    * Add new images and theme files for drug use models
    
    * Update theme paths and remove unused config files
    
    * Add post listing and new ODE solver content
    
    * Update navbar links and remove obsolete ODE files
    
    * Remove About page and update index with new content
    
    * Add D3 graph visualization for tag relationships
    
    * Add Lato font, update tagList and iframe styles
    
    * Fix post links and enhance tag filtering functionality
    
    * Update navbar and fix tag casing in posts
    
    * Add CSS styles for post list layout and typography
    
    * Updating sytles
    
    * Fixed drug use issues
    
    * fix the one of the dev container posts
    
    * Add new Quarto post and update links in existing posts
    
    * Fix list styles and update image path in posts
    
    * fixed the build
    
    * Add new data mining and dev container posts; delete old tip

---
+++ .themes/hello-friend-ng/assets/js/menu.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuTrigger = document.querySelector(".menu-trigger");
+  const menu = document.querySelector(".menu");
+  const breakpoint = 684; // This is the window width threshold, you can adjust this value
+
+  function shouldMenuBeHidden() {
+    return window.innerWidth <= breakpoint;
+  }
+  function updateMenuVisibility() {
+    const shouldBeHidden = shouldMenuBeHidden();
+    menuTrigger.classList.toggle("hidden", !shouldBeHidden);
+    menu.classList.toggle("hidden", shouldBeHidden);
+  }
+
+  updateMenuVisibility();
+
+  menuTrigger?.addEventListener("click", () => {
+    menu?.classList.toggle("hidden");
+  });
+
+  window.addEventListener("resize", updateMenuVisibility);
+
+  document.querySelector(".menu-trigger");
+});
