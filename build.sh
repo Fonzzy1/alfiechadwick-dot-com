19ed49a08367d043c9708e3505e6fc29c823cae0
Fonzzy1 <alfiechadwick@hotmail.com>
2025-03-26 16:13:27 +1100

    Update build.sh dependencies and add interview post

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 pip install matplotlib numpy catppuccin-matplotlib beautifulsoup4==4.12.3 pandas==2.2.0 Requests==2.31.0 lxml
-R -e 'install.packages(c("jsonlite", "xgboost", "remotes"))'
+R -e 'install.packages(c("jsonlite","patchwork", "xgboost", "remotes"))'
 R -e 'remotes::install_github("albert-ying/catppuccin")'
